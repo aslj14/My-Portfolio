a540ecdbaedb1c6cbe0ea8071c5e894ab65b9454
aslj14 <ajo2690@wgu.edu>
2023-06-07 04:05:05 -0500

    Day 21: Add description on the new columns

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 import streamlit as st
 
-st.set_page_config(layout="wide")
-
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image("images/WIN_20230606_13_58_15_Pro.jpg")
+    st.image("images/DwG9sh8I_a.png")
 
 with col2:
     st.title("Ariel Johnson")
@@ -13,3 +11,8 @@ with col2:
     Hi, I am Ariel!
     """
     st.info(content)
+
+content2 = """
+Below you can find some of the apps I have built in Python. Feel free to contact me!
+"""
+st.write(content2)
