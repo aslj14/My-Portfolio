97661a24ed63b2f5d25e4d05020731bfc49ae6d0
aslj14 <ajo2690@wgu.edu>
2023-06-07 06:18:26 -0500

    Day 22: Add app description, image, and links

--- pages/portfolio.py
+++ pages/portfolio.py
@@ -3,14 +3,20 @@ import pandas
 
 st.title("My Portfolio")
 
-col1, col2 = st.columns(2)
+col1, empty_col, col2 = st.columns([1.5, 0.5, 1.5])
 
 df = pandas.read_csv("data.csv", sep=";")
 
 with col1:
     for index, row in df[:10].iterrows():
         st.header(row["title"])
+        st.write(["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
 
 with col2:
     for index, row in df[10:].iterrows():
         st.header(row["title"])
+        st.write(["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
