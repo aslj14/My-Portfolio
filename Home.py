5dd1e5df653022d007fd70dd9a9718891984f5f7
aslj14 <ajo2690@wgu.edu>
2023-06-08 16:55:19 -0500

    Change image

--- Home.py
+++ Home.py
@@ -3,7 +3,7 @@ import streamlit as st
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image("images/DwG9sh8I_a.png")
+    st.image("images/cartoonized_ariel.gif")
 
 with col2:
     st.title("Ariel Johnson")
@@ -12,7 +12,3 @@ with col2:
     """
     st.info(content)
 
-content2 = """
-Below you can find some of the apps I have built in Python. Feel free to contact me!
-"""
-st.write(content2)
